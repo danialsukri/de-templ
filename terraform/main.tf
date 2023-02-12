b2a71079cd037c1372cce4fc11178feb5e8d44b7
danialsukri <123539362+danialsukri@users.noreply.github.com>
2023-02-12 12:27:24 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -128,7 +128,7 @@ EOF
 resource "aws_budgets_budget" "ec2" {
   name              = "budget-ec2-monthly"
   budget_type       = "COST"
-  limit_amount      = "5"
+  limit_amount      = "1"
   limit_unit        = "USD"
   time_period_end   = "2087-06-15_00:00"
   time_period_start = "2022-10-22_00:00"
